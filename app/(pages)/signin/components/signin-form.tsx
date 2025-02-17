607bac8dc3608c1bf3712f12eb7a7383ceaf664d
Arison Firmino <arisonfirmino.dev@gmail.com>
2025-02-17 16:00:14 -0300

    feat: add initial validation to SignInForm in SignIn page

--- app/(pages)/signin/components/signin-form.tsx
+++ app/(pages)/signin/components/signin-form.tsx
@@ -1,15 +1,61 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
 import InputForm from "@/app/components/input-form";
 import SubmitButton from "@/app/components/submit-button";
 
+const schema = yup.object({
+  emailOrUsername: yup
+    .string()
+    .required(
+      "O e-mail ou nome de usuário é obrigatório. Por favor, informe um dos dois.",
+    ),
+  password: yup
+    .string()
+    .required(
+      "A senha é obrigatória. Por favor, insira sua senha para continuar.",
+    ),
+});
+
+type FormData = yup.InferType<typeof schema>;
+
 const SignInForm = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+    reset();
+  };
+
   return (
-    <form className="w-full space-y-3 rounded-2xl border bg-card p-2.5">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-full space-y-3 rounded-2xl border bg-card p-2.5"
+    >
       <InputForm
         label="E-mail ou nome de usuário"
         placeholder="Digite seu e-mail ou nome de usuário"
+        register={{ ...register("emailOrUsername") }}
+        error={errors.emailOrUsername}
       />
 
-      <InputForm type="password" label="Senha" placeholder="Digite sua senha" />
+      <InputForm
+        type="password"
+        label="Senha"
+        placeholder="Digite sua senha"
+        register={{ ...register("password") }}
+        error={errors.password}
+      />
 
       <SubmitButton>Entrar</SubmitButton>
     </form>

5dd864fc2a7327e03b85e66f6b9e994cda3e0969
Arison Firmino <arisonfirmino.dev@gmail.com>
2025-02-17 15:54:42 -0300

    feat: add initial validation to SignUpForm in SignIn page

--- app/(pages)/signin/components/signup-form.tsx
+++ app/(pages)/signin/components/signup-form.tsx
@@ -1,25 +1,101 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
 import InputForm from "@/app/components/input-form";
 import SubmitButton from "@/app/components/submit-button";
 
-const SignUpForm = () => {
-  return (
-    <form className="w-full space-y-3 rounded-2xl border bg-card p-2.5">
-      <div className="flex flex-col gap-3 md:flex-row">
-        <InputForm label="Nome" placeholder="Digite seu nome" />
+const schema = yup.object({
+  firstName: yup
+    .string()
+    .required("Por favor, informe seu nome.")
+    .min(3, "O nome deve ter pelo menos 3 caracteres.")
+    .matches(/^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/, "O nome deve conter apenas letras."),
+  lastName: yup
+    .string()
+    .required("Por favor, informe seu sobrenome.")
+    .min(3, "O sobrenome deve ter pelo menos 3 caracteres.")
+    .matches(
+      /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/,
+      "O sobrenome deve conter apenas letras.",
+    ),
+  username: yup
+    .string()
+    .required("O nome de usuário é obrigatório.")
+    .min(3, "O nome de usuário deve ter pelo menos 3 caracteres.")
+    .matches(/^[a-z0-9._]+$/, "Nome de usuário inválido."),
+  email: yup
+    .string()
+    .required("O email é obrigatório.")
+    .email("Por favor, insira um e-mail válido."),
+  password: yup
+    .string()
+    .required("A senha é obrigatória.")
+    .min(6, "A senha deve ter pelo menos 6 caracteres."),
+  passwordConfirmation: yup
+    .string()
+    .required("A senha deve ter pelo menos 6 caracteres.")
+    .required("A confirmação de senha é obrigatória.")
+    .oneOf(
+      [yup.ref("password")],
+      "As senhas não coincidem. Por favor, verifique.",
+    ),
+});
 
-        <InputForm label="Sobrenome" placeholder="Digite seu sobrenome" />
+type FormData = yup.InferType<typeof schema>;
+
+const SignUpForm = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+    reset();
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-full space-y-3 rounded-2xl border bg-card p-2.5"
+    >
+      <div className="flex flex-col gap-3 md:flex-row">
+        <InputForm
+          label="Nome"
+          placeholder="Digite seu nome"
+          register={{ ...register("firstName") }}
+          error={errors.firstName}
+        />
+
+        <InputForm
+          label="Sobrenome"
+          placeholder="Digite seu sobrenome"
+          register={{ ...register("lastName") }}
+          error={errors.lastName}
+        />
       </div>
 
       <InputForm
         label="Nome de usuário"
         placeholder="Escolha um nome de usuário"
         hint="Pense bem! Este será seu nome fixo na plataforma."
+        register={{ ...register("username") }}
+        error={errors.username}
       />
 
       <InputForm
         label="E-mail"
         placeholder="Digite seu e-mail"
         hint="O e-mail informado será fixo e não poderá ser atualizado."
+        register={{ ...register("email") }}
+        error={errors.email}
       />
 
       <InputForm
@@ -27,12 +103,16 @@ const SignUpForm = () => {
         label="Senha"
         placeholder="Digite sua senha"
         hint="Defina uma senha segura. Ela não poderá ser alterada depois."
+        register={{ ...register("password") }}
+        error={errors.password}
       />
 
       <InputForm
         type="password"
         label="Confirme sua senha"
         placeholder="Digite sua senha novamente"
+        register={{ ...register("passwordConfirmation") }}
+        error={errors.passwordConfirmation}
       />
 
       <SubmitButton>Enviar</SubmitButton>
--- app/components/input-form.tsx
+++ app/components/input-form.tsx
@@ -2,15 +2,21 @@
 
 import { useState } from "react";
 
+import { cn } from "@/app/lib/utils";
+
 import { Label } from "@/app/components/ui/label";
 import { Input } from "@/app/components/ui/input";
 import Hint from "@/app/components/hint";
+
+import { UseFormRegisterReturn, FieldError } from "react-hook-form";
 
 interface InputFormProps {
   label: string;
   type?: string;
   placeholder: string;
   hint?: string;
+  register: UseFormRegisterReturn;
+  error: FieldError | undefined;
 }
 
 const InputForm = ({
@@ -18,6 +24,8 @@ const InputForm = ({
   type = "text",
   placeholder,
   hint,
+  register,
+  error,
 }: InputFormProps) => {
   const [isFocused, setIsFocused] = useState(false);
 
@@ -28,13 +36,21 @@ const InputForm = ({
         <Input
           type={type}
           placeholder={placeholder}
+          {...register}
           onFocus={() => setIsFocused(true)}
           onBlur={() => setIsFocused(false)}
+          className={cn(error && "border-red-600 focus-visible:ring-red-600")}
         />
       ) : (
-        <Input type={type} placeholder={placeholder} />
+        <Input
+          type={type}
+          placeholder={placeholder}
+          {...register}
+          className={cn(error && "border-red-600 focus-visible:ring-red-600")}
+        />
       )}
-      {hint && isFocused && <Hint message={hint} />}
+      {hint && !error && isFocused && <Hint message={hint} />}
+      {error && <p className="text-xs text-red-600">{error.message}</p>}
     </div>
   );
 };

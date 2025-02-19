41efe8c4dee5c1e815297955767736e48dab736f
Arison Firmino <arisonfirmino.dev@gmail.com>
2025-02-19 16:51:58 -0300

    feat: add Loading component to main page to display before page loads

---
+++ app/(pages)/[username]/loading.tsx
@@ -0,0 +1,33 @@
+import { Skeleton } from "@/app/components/ui/skeleton";
+
+const Loading = () => {
+  return (
+    <main className="flex min-h-screen w-full flex-col items-center justify-center gap-5 p-5">
+      <div className="flex items-center gap-5">
+        <Skeleton className="h-[52px] w-[244px]" />
+        <Skeleton className="h-10 w-10" />
+      </div>
+
+      <div className="flex w-full flex-col items-center justify-center gap-5 md:flex-row">
+        <div className="w-full max-w-[448px] space-y-5">
+          <Skeleton className="h-[170px] w-full" />
+          <Skeleton className="h-10 w-full" />
+        </div>
+        <Skeleton className="aspect-square w-full md:max-w-[230px]" />
+      </div>
+
+      <div className="flex items-center gap-1">
+        <Skeleton className="h-10 w-[109px]" />
+
+        <Skeleton className="h-10 w-10" />
+        <Skeleton className="h-10 w-10" />
+        <Skeleton className="h-10 w-10" />
+        <Skeleton className="h-10 w-10" />
+
+        <Skeleton className="h-10 w-[109px]" />
+      </div>
+    </main>
+  );
+};
+
+export default Loading;

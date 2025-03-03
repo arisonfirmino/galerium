d6c50f2aedbeaeda15d4944352e671d258a0e3f9
Arison Firmino <arisonfirmino.dev@gmail.com>
2025-03-03 13:35:14 -0300

    feat: add initial Header component for smaller screens

---
+++ app/components/header.tsx
@@ -0,0 +1,16 @@
+import { SunIcon, UserIcon } from "lucide-react";
+
+const Header = () => {
+  return (
+    <header className="flex items-center justify-between p-5 md:hidden">
+      <h1 className="dancing-script text-2xl font-bold">Galerium</h1>
+
+      <div className="flex items-center gap-5">
+        <SunIcon size={20} />
+        <UserIcon size={20} />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
